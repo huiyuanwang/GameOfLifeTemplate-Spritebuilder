55f3c501e7b633a37347e0964337e16135f74082
Anna Wang <anna.why45@gmail.com>
2015-02-03 20:03:56 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -166,10 +166,12 @@ static const int GRID_COLUMNS = 10;
             
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
-                numAlive ++;
             }
             else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = NO;
+            }
+            if (currentCreature.isAlive == YES) {
+                numAlive ++;
             }
         }
     }
